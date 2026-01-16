c6f32886729a2c3e832e325281aa2f6b7c4efe83
bayubudiana <bayubudiana93@gmail.com>
2026-01-16 11:22:26 +0800

    implementasi caht bot telegram

--- api/send-telegram.js
+++ api/send-telegram.js
@@ -1,23 +1,48 @@
 export default async function handler(req, res) {
-    if(req.method !== 'POST') {
-        return res.status(405).json({ error: 'Method not allowed' });
+  try {
+    if (req.method !== "POST") {
+      return res.status(405).json({ message: "Method not allowed" });
     }
 
-    const {message} = req.body;
+    const { message } = req.body;
 
-    await fetch(
-      `/api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+    if (!message) {
+      return res.status(400).json({ error: "Message is empty" });
+    }
+
+    const token = process.env.TELEGRAM_BOT_TOKEN;
+    const chatId = process.env.TELEGRAM_CHAT_ID;
+
+    if (!token || !chatId) {
+      return res.status(500).json({
+        error: "Telegram env not set",
+      });
+    }
+
+    const tgResponse = await fetch(
+      `https://api.telegram.org/bot${token}/sendMessage`,
       {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          chat_id: process.env.TELEGRAM_CHAT_ID,
+          chat_id: chatId,
           text: message,
         }),
       }
     );
 
-    res.status(200).json({ message: 'Message sent successfully' });
+    const data = await tgResponse.json();
+
+    if (!data.ok) {
+      return res.status(500).json({
+        error: data.description,
+      });
+    }
+
+    return res.json({ success: true });
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
 }
