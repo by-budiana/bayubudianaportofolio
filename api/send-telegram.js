73dfa8e3e90eb57376316c95553cfc876a388318
bayubudiana <bayubudiana93@gmail.com>
2026-01-16 11:00:20 +0800

    implementasi caht bot telegram

--- api/send-telegram.js
+++ api/send-telegram.js
@@ -1,8 +1,19 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
     if(req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
 
-    const {masage} req.body;
+    const {message} = req.body;
 
     await fetch(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',},
+        body: JSON.stringify({
+            chat_id: process.env.TELEGRAM_CHAT_ID,
+            text: message,
+        }),
+    });
+
+    res.status(200).json({ message: 'Message sent successfully' });
+}
